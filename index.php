d85d6ab11409c62032cceccdd6b4220429f35c9c
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-05-27 00:38:13 +0300

    .

--- index.php
+++ index.php
@@ -9,22 +9,22 @@ include_once("config.php");
     <body>
         <h1 style="text-align:center;"><?php echo $title; ?></h1>
         <div style="overflow-x:auto;">
-        <table>
-            <tr>
-                <!--Forum Name-->
-                <th colspan="4">
-                    <p style="font-size: 25px;">Име на форум</p>
-                </th>
-            </tr>
-            <tr>
-                <!--New posts-->
-                <td>X</td>
-                <!--Category (Name/Description)-->
-                <td><p><strong><a href="#">Име на категория</a></strong></p></td>
-                <!--Statistics (Topics/Posts)-->
-                <td><p>Теми:&nbsp;<strong>1</strong></p></td>
-                <td><p>Публикации:&nbsp;<strong>1</strong></p></td>
-        </table>
+            <table>
+                <tr>
+                    <!--Forum Name-->
+                    <th colspan="4">
+                        <p style="font-size: 25px;">Име на форум</p>
+                    </th>
+                </tr>
+                <tr>
+                    <!--New posts-->
+                    <td>X</td>
+                    <!--Category (Name/Description)-->
+                    <td><p><strong><a href="#">Име на категория</a></strong></p></td>
+                    <!--Statistics (Topics/Posts)-->
+                    <td><p>Теми:&nbsp;<strong>1</strong></p></td>
+                    <td><p>Публикации:&nbsp;<strong>1</strong></p></td>
+            </table>
         </div>
     </body>
 </html>
