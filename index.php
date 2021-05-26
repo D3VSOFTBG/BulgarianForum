f138a925cdf5ddad22e1cf6b20815e7e61ecd427
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-05-27 01:17:50 +0300

    .

--- index.php
+++ index.php
@@ -12,8 +12,11 @@ include_once("config.php");
             <table>
                 <tr>
                     <!--Forum Name-->
-                    <th colspan="4">
+                    <th colspan="3">
                         <p style="font-size: 25px;">Име на форум</p>
+                    </th>
+                    <th>
+                        <p><a href="#">[-]</a></p>
                     </th>
                 </tr>
                 <tr>
@@ -22,9 +25,13 @@ include_once("config.php");
                     <!--Category (Name/Description)-->
                     <td><p><strong><a href="#">Име на категория</a></strong></p></td>
                     <!--Statistics (Topics/Posts)-->
-                    <td><p>Теми:&nbsp;<strong>1</strong></p></td>
                     <td><p>Публикации:&nbsp;<strong>1</strong></p></td>
+                    <td><p>Теми:&nbsp;<strong>1</strong></p></td>
+                </tr>                
             </table>
         </div>
+        <footer>
+            <p class="text-center"><a href="#">BulgarianForum</a> &copy; 2021
+        </footer>
     </body>
 </html>
