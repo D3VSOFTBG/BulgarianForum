a9e8fc28378376256a96528326ead79a53029f42
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-06-06 21:16:48 +0300

    .

--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,10 @@
 <footer>
-        <p class="text-center"><a href="#">BulgarianForum</a> &copy; 2021</p>
-        <p style="color: gray;" class="text-center">Текущо време: (<strong id="time"></strong>)</p>
-    </footer>
-    <!--JavaScript-->
-    <script src="<?php echo $url; ?>/assets/js/time.js"></script>
+    <p class="text-center"><a href="#">BulgarianForum</a> &copy; 2021</p>
+    <p style="color: gray;" class="text-center">Текущо време: (<strong id="time"></strong>)</p>
+</footer>
+<!--JavaScript-->
+<script src="<?php echo $url; ?>/assets/js/time.js"></script>
+<script src="<?php echo $url; ?>/assets/js/nav.js"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -3,11 +3,14 @@ include_once("include/header.php");
 ?>
 <h1 style="text-align:center;"><?php echo $title; ?></h1>
 
-<nav>
-    <a href="auth/register.html">Регистрация</a>
-    <a href="auth/login.html">Вход</a>
-    <a href="#">Търси</a>
-    <img src="<?php echo $url; ?>/assets/images/menu.png" alt="" srcset="">
+<nav class="topnav" id="myTopnav">
+    <a href="javascript:void(0);" class="icon" onclick="nav()">
+        <img src="<?php echo $url; ?>/assets/images/nav.png" />&nbsp;НАВИГАЦИЯ
+    </a>
+    <a href="#home">Home</a>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <a href="#about">About</a>
 </nav>
 <!--<input type="search" placeholder="Търси" style="padding: 10px;" />-->
 <div style="overflow-x:auto;">
