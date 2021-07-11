474f979443520f29d9a7f9f4008a2037152193e1
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-07-11 23:25:32 +0300

    .

--- assets/js/date.js
+++ assets/js/date.js
@@ -1,3 +1,3 @@
 var today = new Date();
 var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
-document.getElementById("time").innerHTML = date;
+document.getElementById("date").innerHTML = date;
--- include/footer.php
+++ include/footer.php
@@ -1,9 +1,9 @@
 <footer>
     <p class="text-center"><a href="#">BulgarianForum</a> &copy; 2021</p>
-    <p style="color: gray;" class="text-center">Текущо време: (<strong id="time"></strong>)</p>
+    <p style="color: gray;" class="text-center">Дата: (<strong id="date"></strong>)</p>
 </footer>
 <!--JavaScript-->
-<script src="<?php echo $url; ?>/assets/js/time.js"></script>
+<script src="<?php echo $url; ?>/assets/js/date.js"></script>
 <script src="<?php echo $url; ?>/assets/js/nav.js"></script>
 </body>
 
