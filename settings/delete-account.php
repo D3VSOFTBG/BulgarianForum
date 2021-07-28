507672b615b2748d9c07405aa8accb8c161a7460
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-07-29 00:52:23 +0300

    udari we

--- settings/delete-account.php
+++ settings/delete-account.php
@@ -2,6 +2,12 @@
 // Initialize the session
 session_start();
 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../auth/login.php");
+    exit;
+}
+
 // Include database file
 require_once("../include/db.php");
 
