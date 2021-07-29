b8764aba11ec3ccd3c617cc9a5e33fef0278aa11
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-07-29 04:53:51 +0300

    Update forgot-password.php

--- auth/forgot-password.php
+++ auth/forgot-password.php
@@ -15,11 +15,45 @@ require_once("../include/db.php");
 include_once("../include/header.php");
 
 // Define variables and initialize with empty values
-$email = "";
-$email_err = "";
+$email = $captcha = "";
+$email_err = $captcha_err = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty(trim(htmlspecialchars($_POST["captcha"])))){
+        $captcha_err = "Моля препишете буквите.";
+    }else{
+        $captcha = trim(htmlspecialchars($_POST["captcha"]));
+        if(isset($captcha) && $captcha == $_SESSION["CAPTCHA_TEXT"]){
+            // Check if email is empty
+            if(empty(trim(htmlspecialchars($_POST["email"])))){
+                $email_err = "Моля въведете вашият имейл.";
+            }else{
+                $email = trim(htmlspecialchars($_POST["email"]));
+            }
 
+            // Validate credentials
+            if(empty($email_err)){
+                // Prepare select statement
+                $sql = "SELECT email FROM users WHERE email = :email";
+
+                if($stmt = $pdo->prepare($sql)){
+                    // Bind variables to the prepared statement as parameters
+                    $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
+
+                    // Set parameters
+                    $param_email = trim(htmlspecialchars($_POST["email"]));
+
+                    // Attempt to execute the prepared statement
+                    if($stmt->execute()){
+                        // Check if email exist, if yes then send key via mail
+                        if($stmt->rowCount() == 1){
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 ?>
 <div class="text-center border">
