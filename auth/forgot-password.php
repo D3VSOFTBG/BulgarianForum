dfa506142f9ab8aab0767b9b2e9b161119424c8d
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-07-30 02:46:36 +0300

    Update forgot-password.php

--- auth/forgot-password.php
+++ auth/forgot-password.php
@@ -37,7 +37,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Validate credentials
             if(empty($email_err)){
                 // Prepare select statement
-                $sql = "SELECT email FROM users WHERE email = :email";
+                $sql = "SELECT email, username FROM users WHERE email = :email";
 
                 if($stmt = $pdo->prepare($sql)){
                     // Bind variables to the prepared statement as parameters
@@ -50,30 +50,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                     if($stmt->execute()){
                         // Check if email exist, if yes then send Token via mail
                         if($stmt->rowCount() == 1){
-                            // Token
-                            $token = md5(substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") , 0 , 62));
-                            $pdo->prepare("UPDATE users SET token = ?, token_created_time = ? WHERE email = ?")->execute([$token, time(), $email]);
+                            if($row = $stmt->fetch()){
+                                $username = $row["username"];
+
+                                // Token
+                                $token = md5(substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") , 0 , 62));
+                                $pdo->prepare("UPDATE users SET token = ?, token_created_time = ? WHERE email = ?")->execute([$token, time(), $email]);
                             
-                            // Email
-                            require '../vendor/autoload.php';
-                            $mail = new PHPMailer;
-                            $mail->CharSet = "UTF-8";
-                            $mail->isSMTP();
-                            $mail->Host = $smtp_host;
-                            $mail->Port = $smtp_port;
-                            $mail->SMTPAuth = true;
-                            $mail->Username = $smtp_username;
-                            $mail->Password = $smtp_password;
-                            $mail->setFrom($smtp_from_email, $smtp_from_username);
-                            $mail->addAddress($email);
-                            if ($mail->addReplyTo($email)) {
-                                $mail->Subject = "$title - Забравена парола";
-                                $mail->isHTML(false);
-                                $mail->Body = <<<EOT
-                                Token: $token
-                                EOT;
-                                $mail->send();
-                                $sent_message = "Проверете си имейла!";
+                                // Email
+                                require '../vendor/autoload.php';
+                                $mail = new PHPMailer;
+                                $mail->CharSet = "UTF-8";
+                                $mail->isSMTP();
+                                $mail->Host = $smtp_host;
+                                $mail->Port = $smtp_port;
+                                $mail->SMTPAuth = true;
+                                $mail->Username = $smtp_username;
+                                $mail->Password = $smtp_password;
+                                $mail->setFrom($smtp_from_email, $smtp_from_username);
+                                $mail->addAddress($email);
+                                if ($mail->addReplyTo($email)) {
+                                    $mail->Subject = "$title - Забравена парола";
+                                    $mail->isHTML(false);
+                                    $mail->Body = <<<EOT
+                                    Username: $username
+                                    Token: $token
+                                    EOT;
+                                    $mail->send();
+                                    $sent_message = "Проверете си имейла!";
+                                }
                             }
                         }else{
                             // To prevent abuse
