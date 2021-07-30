42d199a9804fc85194882496b6beb221b34a8342
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-07-30 04:40:08 +0300

    Update change-password.php

--- settings/change-password.php
+++ settings/change-password.php
@@ -12,20 +12,22 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 include_once("../include/header.php");
 ?>
 <div class="text-center border">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
     <h1>Промени паролата</h1>
     <label>Текуща парола</label>
     <br />
-    <input placeholder="Текуща парола" required />
+    <input type="password" name="password" placeholder="Текуща парола" required />
     <br />
     <label>Нова парола</label>
     <br />
-    <input placeholder="Нова парола" required />
+    <input type="password" name="new_password" placeholder="Нова парола" required />
     <br />
     <label>Потвърдете новата парола</label>
     <br />
-    <input placeholder="Потвърдете новата парола" required />
+    <input type="password" name="confirm_new_password" placeholder="Потвърдете новата парола" required />
     <br />
     <a href="index.php"><button type="button">Назад</button><button>Промени</button>
+    </form>
 </div>
 <?php
 include_once("../include/footer.php");
