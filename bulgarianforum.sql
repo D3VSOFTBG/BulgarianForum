3cba4f6c73b9e9890a78f6b2bb5a168a20123a83
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-08-01 01:33:54 +0300

    Update bulgarianforum.sql

--- bulgarianforum.sql
+++ bulgarianforum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране: 29 юли 2021 в 23:45
+-- Време на генериране:  1 авг 2021 в 00:33
 -- Версия на сървъра: 10.4.20-MariaDB
 -- Версия на PHP: 8.0.8
 
@@ -33,6 +33,7 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `registered_date` date NOT NULL,
+  `profile_picture` varchar(250) DEFAULT NULL,
   `token` varchar(50) DEFAULT NULL,
   `token_created_time` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -41,14 +42,15 @@ CREATE TABLE `users` (
 -- Схема на данните от таблица `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `registered_date`, `token`, `token_created_time`) VALUES
-(3, 'test2', 'test2@gmail.com', '$2y$10$nX6IM/B2xycREmZ5c7MJmeeuOHj1A2e5oF4tevj9vi6GwBc.7mZkK', '2021-06-26', '104b5151221169c35fe498b64c7aab7a', NULL),
-(4, 'testing123', 'testing123@gmail.com', '$2y$10$PnoD9whra37uaElh5X0w6uupq2nrELXRXByj4sMvPYDuQ4wWgFMda', '2021-06-30', 'asd1231323', NULL),
-(8, 'parapetXD123', 'parapetXD123@gmail.com', '$2y$10$qAN8j1OxvLbJJoyZOBiEN.kZrAC1YZMQ7nl7zJngUgk/kK0uKHc8O', '2021-07-27', NULL, NULL),
-(9, 'leshtorba123', 'leshtorba123@gmail.com', '$2y$10$cmSd.GmeJ4LU/UO5AKZ4k.4TrWJuMGUvlcLbh8AdYkO/vProsyDQ.', '2021-07-27', NULL, NULL),
-(10, 'parolata', 'parolata@gmail.com', '$2y$10$6szoppCa3EyDd0P7FOcgA.u.VvHDfsv6M6QCyRXsFqAMNMaH7S4yK', '2021-07-27', NULL, NULL),
-(11, 'testvam123', 'testvam123@gmail.com', '$2y$10$weVdAg5ZGmQ7vJt66dqsde4o5KDplCvRFMV4aOjMtEPfnnGOZur7G', '2021-07-28', NULL, NULL),
-(12, 'proba123', 'proba123@gmail.com', '$2y$10$HfQ58v1qFGT8yUqgznpFSO9pKxCJB6PuQCYPPukFMrLHlcoJcpKqS', '2021-07-28', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `registered_date`, `profile_picture`, `token`, `token_created_time`) VALUES
+(3, 'test2', 'test2@gmail.com', '$2y$10$nX6IM/B2xycREmZ5c7MJmeeuOHj1A2e5oF4tevj9vi6GwBc.7mZkK', '2021-06-26', NULL, '104b5151221169c35fe498b64c7aab7a', NULL),
+(4, 'testing123', 'testing123@gmail.com', '$2y$10$kjhTbfv89SvmnFI1t1KYqufXAOq/hSxhb4ZQ/y7FaNBx/EWESadSa', '2021-06-30', NULL, NULL, NULL),
+(8, 'parapetXD123', 'parapetXD123@gmail.com', '$2y$10$tgl2YgmUDInfdXnTk9OsbuBEOd8oiPKIeYecd.HsFlrFeKMvyC0.y', '2021-07-27', NULL, NULL, NULL),
+(9, 'leshtorba123', 'leshtorba123@gmail.com', '$2y$10$cmSd.GmeJ4LU/UO5AKZ4k.4TrWJuMGUvlcLbh8AdYkO/vProsyDQ.', '2021-07-27', NULL, 'aa2127c15497b283978e41e90d16aaee', 1627602360),
+(10, 'parolata', 'parolata@gmail.com', '$2y$10$6szoppCa3EyDd0P7FOcgA.u.VvHDfsv6M6QCyRXsFqAMNMaH7S4yK', '2021-07-27', NULL, '1654e0c2d67687ea84e695c5dff7b91b', 1627597381),
+(11, 'testvam123', 'testvam123@gmail.com', '$2y$10$weVdAg5ZGmQ7vJt66dqsde4o5KDplCvRFMV4aOjMtEPfnnGOZur7G', '2021-07-28', NULL, NULL, NULL),
+(12, 'proba123', 'proba123@yahoo.com', '$2y$10$nkyuiAi5zdRXyFafw7PLAuLCXh8m.Nn0c5eU.IJOWrnN/fewxYuRW', '2021-07-28', '../uploads/proba123/profile-picture/ed0a3721e8b4bdb2122713ea7fa248eb.jpg', NULL, NULL),
+(13, 'nevuzmojen123', 'nevuzmojen123@gmail.com', '$2y$10$3DUlIs7HP2/iZiz17vcOgeQF68RQUTg9LEbBQiaAejtZiBkrJTU2i', '2021-07-30', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -70,7 +72,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
