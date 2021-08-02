06c13c9ae7eeda85470f60b94f0ae719734cf03b
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-08-02 22:40:02 +0300

    Update bulgarianforum.sql

--- bulgarianforum.sql
+++ bulgarianforum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране:  2 авг 2021 в 20:51
+-- Време на генериране:  2 авг 2021 в 21:39
 -- Версия на сървъра: 10.4.20-MariaDB
 -- Версия на PHP: 8.0.8
 
@@ -35,6 +35,7 @@ CREATE TABLE `users` (
   `registered_date` date NOT NULL,
   `role` varchar(50) NOT NULL,
   `profile_picture` varchar(250) NOT NULL,
+  `email_confirmed` int(11) NOT NULL,
   `token` varchar(50) DEFAULT NULL,
   `token_created_time` int(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -43,9 +44,10 @@ CREATE TABLE `users` (
 -- Схема на данните от таблица `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `registered_date`, `role`, `profile_picture`, `token`, `token_created_time`) VALUES
-(12, 'proba123', 'proba123@yahoo.com', '$2y$10$nkyuiAi5zdRXyFafw7PLAuLCXh8m.Nn0c5eU.IJOWrnN/fewxYuRW', '2021-07-28', 'Administrator', '../uploads/proba123/profile-picture/ed0a3721e8b4bdb2122713ea7fa248eb.jpg', NULL, NULL),
-(14, 'novpotrebitel123', 'novpotrebitel123@gmail.com', '$2y$10$YOwj6GnAX0df8geebxRO5OjNvAh3nGCB/K9qDEpdHDWxaZAV3KqUi', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/26cbe35a41c2fdc83515b1236dc65509', NULL, NULL);
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `registered_date`, `role`, `profile_picture`, `email_confirmed`, `token`, `token_created_time`) VALUES
+(12, 'proba123', 'proba123@yahoo.com', '$2y$10$nkyuiAi5zdRXyFafw7PLAuLCXh8m.Nn0c5eU.IJOWrnN/fewxYuRW', '2021-07-28', 'Administrator', '../uploads/proba123/profile-picture/ed0a3721e8b4bdb2122713ea7fa248eb.jpg', 0, NULL, NULL),
+(14, 'novpotrebitel123', 'novpotrebitel123@gmail.com', '$2y$10$YOwj6GnAX0df8geebxRO5OjNvAh3nGCB/K9qDEpdHDWxaZAV3KqUi', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/26cbe35a41c2fdc83515b1236dc65509', 0, NULL, NULL),
+(15, 'petel123', 'petel123@gmail.com', '$2y$10$mpAj26DD3SH1a7ZWekh7Yuv8NuVT7./82FZAG8bBrgIm33jUAHozC', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/b5339c141ece5ead023b6f98745f1eb0', 0, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -67,7 +69,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
