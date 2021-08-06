b8e0319187c545aa4d707bb6aa4d6d488ddfe2c0
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-08-07 02:31:55 +0300

    delete-category.php added

--- admin/categories.php
+++ admin/categories.php
@@ -18,15 +18,30 @@ require_once("../include/db.php");
 
 // Include header file
 include_once("../include/header.php");
+
+// Define variables and initialize with empty values
+$categories = "";
+
+// Prepare a select statement
+$sql = "SELECT category_name FROM categories";
+
+if($stmt = $pdo->prepare($sql)){
+    if($stmt->execute()){
+        $result = $stmt->fetchAll();
+    }
+}
 ?>
 
 <div class="text-center border">
     <h1>Категории</h1>
     <div class="responsive">
     <table>
+        <?php
+        foreach($result as $row){
+        ?>
         <tr>
             <td>
-                Име_на_категория
+                <?php echo $row["category_name"]; ?>
             </td>
             <td>
                 <button>Редактирай</button>
@@ -35,6 +50,9 @@ include_once("../include/header.php");
                 <button>Изтрий</button>
             </td>
         </tr>
+        <?php
+        }
+        ?>
     </table>
     </div>
     <a href="index.php"><button>Назад</button></a><a href="create-category.php"><button>Създай категория</button></a>
---
+++ admin/delete-category.php
@@ -0,0 +1,42 @@
+<?php
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: ../auth/login.php");
+    exit;
+}else{
+    if($_SESSION["role"] !== "Administrator"){
+        header("location: ../index.php");
+        exit;
+    }
+}
+
+// Include database file
+require_once("../include/db.php");
+
+if(isset($_GET["id"])){
+    // Delete statement
+    $sql = "DELETE FROM categories WHERE id = :id";
+    
+    if($stmt = $pdo->prepare($sql)){
+        // Bind variables to the prepared statement as parameters
+        $stmt->bindParam(":id", $param_id, PDO::PARAM_INT);
+        
+        // Set parameters
+        $param_id = trim(htmlspecialchars($_GET["id"]));
+
+        // Attempt to execute the prepared statement
+        if($stmt->execute()){
+            echo "<script>alert('КАТЕГОРИЯТА Е ИЗТРИТА УСПЕШНО!');window.history.back();</script>";
+        }else{
+            echo "Грешка, моля опитайте по късно";
+        }
+        // Close statement
+        unset($stmt);
+    }
+}
+// Close connection
+unset($pdo);
+?>
