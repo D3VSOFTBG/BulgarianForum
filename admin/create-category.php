0aa278c2359b6a6136db58ed1218bdf334cbd5ff
D3VBG <59423787+D3VBG@users.noreply.github.com>
2021-08-07 01:40:03 +0300

    create-category updated

--- admin/create-category.php
+++ admin/create-category.php
@@ -18,14 +18,78 @@ require_once("../include/db.php");
 
 // Include header file
 include_once("../include/header.php");
+
+// Define variables and initialize with empty values
+$category_name = "";
+$category_name_err = "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(empty(trim(htmlspecialchars($_POST["category_name"])))){
+        $category_name_err = "Моля въведете име на категория.";
+    }else{
+        // Prepare a select statement
+        $sql = "SELECT category_name FROM categories WHERE category_name = :category_name";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":category_name", $param_category_name, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_category_name = trim(htmlspecialchars($_POST["category_name"]));
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                if($stmt->rowCount()){
+                    $category_name_err = "Тази категория вече съществува.";
+                }else{
+                    $category_name = trim(htmlspecialchars($_POST["category_name"]));
+                }
+            }else{
+                echo "Грешка, моля опитайте по късно";
+            }
+            // Close statement
+            unset($stmt);
+        }
+    }
+    // Check input errors before inserting in database
+    if(empty($category_name_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO categories (category_name) VALUES (:category_name)";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":category_name", $param_category_name, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_category_name = $category_name;
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                echo "<script>alert('КАТЕГОРИЯТА Е СЪЗДАДЕНА УСПЕШНО!');</script>";
+            }else{
+                echo "Грешка, моля опитайте по късно.";
+            }
+            // Close statement
+            unset($stmt);
+        }
+    }
+
+    // Close connection
+    unset($pdo);
+}
 ?>
 
 <div class="text-center border">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <h1>Създай категория</h1>
-        <label>Име на категория</label>
+        <label for="category_name">Име на категория</label>
+        <?php
+            if(!empty($category_name_err)){
+                echo '<br /><span class="error">'.$category_name_err.'</span>';
+            }
+        ?>
         <br />
-        <input type="text" name="category_name" placeholder="Име на категория" required>
+        <input type="text" id="category_name" name="category_name" placeholder="Име на категория" required>
         <br />
         <a href="index.php"><button type="button">Назад</button></a><button type="submit">Създай категория</button>
     </form>
--- bulgarianforum.sql
+++ bulgarianforum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Време на генериране:  2 авг 2021 в 21:39
+-- Време на генериране:  7 авг 2021 в 00:17
 -- Версия на сървъра: 10.4.20-MariaDB
 -- Версия на PHP: 8.0.8
 
@@ -20,6 +20,17 @@ SET time_zone = "+00:00";
 --
 -- База данни: `bulgarianforum`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -45,13 +56,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `registered_date`, `role`, `profile_picture`, `email_confirmed`, `token`, `token_created_time`) VALUES
-(12, 'proba123', 'proba123@yahoo.com', '$2y$10$nkyuiAi5zdRXyFafw7PLAuLCXh8m.Nn0c5eU.IJOWrnN/fewxYuRW', '2021-07-28', 'Administrator', '../uploads/proba123/profile-picture/ed0a3721e8b4bdb2122713ea7fa248eb.jpg', 0, NULL, NULL),
-(14, 'novpotrebitel123', 'novpotrebitel123@gmail.com', '$2y$10$YOwj6GnAX0df8geebxRO5OjNvAh3nGCB/K9qDEpdHDWxaZAV3KqUi', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/26cbe35a41c2fdc83515b1236dc65509', 0, NULL, NULL),
-(15, 'petel123', 'petel123@gmail.com', '$2y$10$mpAj26DD3SH1a7ZWekh7Yuv8NuVT7./82FZAG8bBrgIm33jUAHozC', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/b5339c141ece5ead023b6f98745f1eb0', 0, NULL, NULL);
+(12, 'proba123', 'proba123@yahoo.com', '$2y$10$nkyuiAi5zdRXyFafw7PLAuLCXh8m.Nn0c5eU.IJOWrnN/fewxYuRW', '2021-07-28', 'Administrator', '../uploads/proba123/profile-picture/ed0a3721e8b4bdb2122713ea7fa248eb.jpg', 1, NULL, NULL),
+(14, 'novpotrebitel123', 'novpotrebitel123@gmail.com', '$2y$10$YOwj6GnAX0df8geebxRO5OjNvAh3nGCB/K9qDEpdHDWxaZAV3KqUi', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/26cbe35a41c2fdc83515b1236dc65509', 0, '9d25523a5ec1325b122918a28dc84d17', 1628037653),
+(15, 'petel123', 'petel123@gmail.com', '$2y$10$mpAj26DD3SH1a7ZWekh7Yuv8NuVT7./82FZAG8bBrgIm33jUAHozC', '2021-08-02', 'Member', 'https://www.gravatar.com/avatar/b5339c141ece5ead023b6f98745f1eb0', 0, '8049c55e43059b3aac877c3ee2dcfd0e', 1627945387);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Индекси за таблица `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индекси за таблица `users`
@@ -64,6 +81,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
